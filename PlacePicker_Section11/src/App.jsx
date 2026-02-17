3941e06a92c2292d4c7166b98cdceb8de13fe337
Anjali Valani <anjalivalani728@gmail.com>
2026-02-17 18:49:51 +0530

    Add useEffect to get rid of sideEffect

---
+++ PlacePicker_Section11/src/App.jsx
@@ -0,0 +1,97 @@
+import { useRef, useState,useEffect } from "react";
+
+import Places from "./components/Places";
+import { AVAILABLE_PLACES } from "./data";
+import Modal from "./components/Modal";
+import DeleteConfirmation from "./components/DeleteConfirmation";
+import logoImg from "./assets/logo.png";
+import { sortPlacesByDistance } from "./Ioc";
+
+function App() {
+  const modal = useRef();
+  const selectedPlace = useRef();
+  const [availblePlaces, setAvailablePlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState([]);
+
+  // ///////////////////////////use useeffect to get rid of this infinite loop problem of sideeffect
+  // navigator.geolocation.getCurrentPosition((position) => {
+  //   const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude);
+  //   setAvailablePlaces(sortedPlaces)
+  // });
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// solution : useeffect  !st param. sideeffect function, 2nd param. dependency array
+useEffect(()=>{
+ navigator.geolocation.getCurrentPosition((position) => {
+    const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude);
+    setAvailablePlaces(sortedPlaces)
+  });
+},[])
+
+  function handleStartRemovePlace(id) {
+    modal.current.open();
+    selectedPlace.current = id;
+  }
+
+  function handleStopRemovePlace() {
+    modal.current.close();
+  }
+
+  function handleSelectPlace(id) {
+    setPickedPlaces((prevPickedPlaces) => {
+      if (prevPickedPlaces.some((place) => place.id === id)) {
+        return prevPickedPlaces;
+      }
+      const place = AVAILABLE_PLACES.find((place) => place.id === id);
+      return [place, ...prevPickedPlaces];
+    });
+    const storedIds=JSON.parse(localStorage.getItem('selectedPlaces'))||[];
+    if(storedIds.indexOf(id)===-1){
+      localStorage.setItem('selectedPlace',JSON.stringify([id,...storedIds]));
+    }
+    
+  }
+
+  function handleRemovePlace() {
+    setPickedPlaces((prevPickedPlaces) =>
+      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
+    );
+    modal.current.close();
+  }
+
+  return (
+    <>
+      <Modal ref={modal}>
+        <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />
+      </Modal>
+
+      <header>
+        <img src={logoImg} alt="Stylized globe" />
+        <h1>PlacePicker</h1>
+        <p>
+          Create your personal collection of places you would like to visit or
+          you have visited.
+        </p>
+      </header>
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          fallbackText={"Select the places you would like to visit below."}
+          places={pickedPlaces}
+          onSelectPlace={handleStartRemovePlace}
+        />
+        <Places
+          title="Available Places"
+          places={availblePlaces}
+          fallbackText={"Sorting places by distance..."}
+          onSelectPlace={handleSelectPlace}
+        />
+      </main>
+    </>
+  );
+}
+
+export default App;
