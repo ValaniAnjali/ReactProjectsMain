dc8f4a127668b4aaeed2a9fcea9d0a4e6de5e568
Anjali Valani <anjalivalani728@gmail.com>
2026-02-18 18:34:34 +0530

    proper use of useCallback

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,21 +1,23 @@
-import { useState } from "react";
-import Child from "./Child";
+import { useState, useEffect,useCallback } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [query, setQuery] = useState("react");
+  const [count,setCount]=useState(0);
+  const fetchData=useCallback(function fetchData2() {
+    console.log("Fetching for:", query);
+    setQuery("java");
+  },[query]) 
 
-  console.log("Parent rendered");
-
-  return (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>
-        Increase Count
-      </button>
-
-      <Child />
-    </div>
-  );
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);   // ⚠️ dependency
+  return(
+  <>
+  <p>
+    <button onClick={()=>{setCount(count+1)}}>{count}</button>
+  </p>
+  </>
+)
 }
 
 export default App;
