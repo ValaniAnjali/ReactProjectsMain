4df49e259e8be0e2e263fd0e016b9fc729d910ec
Anjali Valani <anjalivalani728@gmail.com>
2026-02-26 12:45:22 +0530

    Add FoodOrder App from tutorial section19

---
+++ section18_Tutorial/01-starting-project/src/components/Meals.jsx
@@ -0,0 +1,30 @@
+
+import MealItem from './MealItem';
+import useHttp from '../hooks/useHttp';
+import Error from './Error';
+const requestConfig={};
+const Meals = () => {
+    const {data:loadedMeals,isLoading,error}=useHttp('http://localhost:3000/mealssss',requestConfig,[]);
+    console.log(loadedMeals);
+
+    if(isLoading){
+        return <p className='center'>Fetching meals...</p>
+    }
+
+    if(error){
+        return <Error title="Failed to fetch meals" message={error}/>
+    }
+    
+    return(
+        <ul id="meals">
+            {
+                loadedMeals.map((meal)=>(
+                    <MealItem key={meal.id} meal={meal}/>
+                ))
+            }
+        </ul>
+    );
+  
+}
+
+export default Meals
